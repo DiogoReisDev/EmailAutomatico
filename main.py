ea9f87ef78523b5a5362f8bd3bf8fa348cf2175a
Diogo Santana <diogo.anatnas@gmail.com>
2023-04-16 20:46:16 -0300

    Criando os envios de email.

---
+++ main.py
@@ -0,0 +1,20 @@
+import win32com.client as win32
+
+# conectar código ao outlook
+outlook = win32.Dispatch('outlook.application')
+# Criar um email
+email = outlook.CreateItem(0)
+# detalhes do email
+email.To = 'mvictoriasilva114@gmail.com'#Destino
+email.Subject = 'Testando código'#Assunto
+email.HTMLBody = '''
+<p>Olá Victória, estou testando meu código!</p>
+
+<p>De acordo com o python, o envio de email soclicitado funcionou!</p>
+
+<p>Atenciosamente,</p>
+<p>Diogo Santana</p>
+'''
+
+email.Send()
+print('Email enviado!')
